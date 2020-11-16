b6cfce6afc246ff60728cec44252931768e875e7
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-11-15 20:36:16 -0500

    added post action

--- src/Actions/releaseAction.js
+++ src/Actions/releaseAction.js
@@ -7,3 +7,21 @@ export const fetchReleases = () => {
       })
     }
   }
+
+  export const addRelease = (release, history) => {
+    return (dispatch) => {
+      fetch('http://localhost:3000/releases', {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accepts': 'application/json'
+        },
+        body: JSON.stringify(release)
+      })
+      .then(resp => resp.json())
+      .then(newRelease => {
+        dispatch({type: 'ADD_RELEASE', payload: newRelease })
+        history.push(`/release-forms/${newRelease.id}`)
+      })
+    }
+  }
