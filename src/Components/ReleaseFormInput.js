282583a541f1a0b38af4cbfc8115307dcc23d94f
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-11-15 20:18:47 -0500

    func to adjust state of newrelease component

--- src/Components/ReleaseFormInput.js
+++ src/Components/ReleaseFormInput.js
@@ -13,25 +13,35 @@ export default class ReleaseFormInput extends Component {
     additionalNotes: ''
   }
 
+  handleOnChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+}
+
   
   render() {
     return (
+      <div>
         <Form >
           <Form.Row>
             <Form.Group as={Col} md="3" controlId="validationFormik101">
               <Form.Label>First name</Form.Label>
-              <Form.Control type="text" name="firstName" placeholder="First Name:"/>
+              <Form.Control type="text" name="firstName" placeholder="First Name:" 
+              onChange={(event) => this.handleOnChange(event)}/>
             </Form.Group>
 
             <Form.Group as={Col} md="3" controlId="validationFormik102">
               <Form.Label>Last name</Form.Label>
-              <Form.Control type="text" name="lastName" placeholder="Last Name:" />
+              <Form.Control type="text" name="lastName" placeholder="Last Name:" 
+              onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group as={Col} md="6" controlId="validationFormikUsername2">
               <Form.Label>Client Email</Form.Label>
               <InputGroup>
-                <Form.Control type="text" placeholder="Client Email:" name="client_email" />
+                <Form.Control type="text" placeholder="Client Email:" name="clientEmail" 
+                onChange={(event) => this.handleOnChange(event)} />
               </InputGroup>
             </Form.Group>
 
@@ -40,26 +50,31 @@ export default class ReleaseFormInput extends Component {
             <Form.Group as={Col} md="4" controlId="validationFormik103">
               <Form.Label>Job Title</Form.Label>
               <Form.Control
-                type="text" placeholder="Job Title:" name="job_title"/>
+                type="text" placeholder="Job Title:" name="jobTitle" 
+                onChange={(event) => this.handleOnChange(event)}/>
             </Form.Group>
 
             <Form.Group as={Col} md="8" controlId="validationFormik104">
               <Form.Label>Description</Form.Label>
-              <Form.Control type="text" placeholder="Description:"/>
+              <Form.Control type="text" placeholder="Description:" name="jobDescription" 
+              onChange={(event) => this.handleOnChange(event)}/>
             </Form.Group>
 
             <Form.Group as={Col} md="3" controlId="validationFormik105">
               <Form.Label>Date Due</Form.Label>
-              <Form.Control type="text" placeholder="09-20-2021" name="due_date"/>
+              <Form.Control type="text" placeholder="09-20-2021" name="dueDate" 
+              onChange={(event) => this.handleOnChange(event)}/>
             </Form.Group>
 
             <Form.Group as={Col} md="6" controlId="validationFormik105">
               <Form.Label>Additional Notes</Form.Label>
-              <Form.Control type="text" placeholder="Additional Notes:" name="due_date"/>
+              <Form.Control type="text" placeholder="Additional Notes:" name="additionalNotes" 
+              onChange={(event) => this.handleOnChange(event)}/>
             </Form.Group>
             
           </Form.Row>
           <Button type="submit">Submit form</Button>
         </Form>
+      </div>
       )}
     }
